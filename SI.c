e80587c5ec31ec592f480aacb4266833ab9487ee
Dash-Nivedita2000 <140108177+Dash-Nivedita2000@users.noreply.github.com>
2023-07-21 16:20:29 +0530

    Add files via upload

---
+++ SI.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	float P,t,r,si;
+	printf(" enter the value of  p,t,r\n");
+	scanf("%f%f%f",&P,&t,&r);
+	si=(P*t*r/100);
+	printf("simple interest is %f",si);
+	
+	return 0;
+	}
+
+
